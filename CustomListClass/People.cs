e2ed8c774a0a1b4b3ac3e229f084c6986f7ba26c
ton <bojingo10@gmail.com>
2016-12-12 20:19:41 -0600

    added methods

---
+++ CustomListClass/People.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomListClass
+{
+    class People
+    {
+        public People[]
+        public string firstName;
+        public string lastName;
+        public string typeOfPet;
+        public string favoriteColor;
+        public int numberOfChildren;
+        public int numberOfFamilyMembers;
+        public int numberOfPets;
+        public int age;
+        public int favoriteNumber;
+        public int numberOf;
+
+        public People()
+        {
+        }
+        public void AddInformation()
+        {
+            int i;
+            for (i = 0; i < 10; i++);
+
+        }
+        public string AddFirstName()
+        {
+            firstName = "Ton";           
+            return firstName;
+            
+        } 
+        public string AddLastName()
+        {
+            lastName = "Vang";
+            return lastName;
+        } 
+        public string AddTypeOfPet()
+        {
+            typeOfPet = "Dog";
+            return typeOfPet;
+        }
+        public string AddFavoriteColor()
+        {
+            favoriteColor = "Green";
+            return favoriteColor;
+        }
+        public int AddNumberOfChildren()
+        {
+            numberOfChildren = Convert.ToInt32("3");
+            return numberOfChildren;
+        }
+            public int AddNumberOfFamilyMembers()
+        {
+            numberOfFamilyMembers = Convert.ToInt32("5");
+            return numberOfFamilyMembers;
+        }
+            public int AddNumberOfPets()
+        {
+            numberOfPets = Convert.ToInt32("1");
+            return numberOfPets;
+        }
+        public int AddAge()
+        {
+            age = Convert.ToInt32("28");
+            return age;
+        }
+        public int AddFavoriteNumber()
+        {
+            favoriteNumber = Convert.ToInt32("10");
+            return favoriteNumber;
+        }
+        public int 
+    }
+}
--- CustomListClass/Program.cs
+++ CustomListClass/Program.cs
@@ -10,6 +10,14 @@ namespace CustomListClass
     {
         static void Main(string[] args)
         {
+            People people = new People();
+            People[] information = new People[10];
+            name[0] = people.AddFirstName();
+            name[1] = people.AddLastName();
+            Console.WriteLine(people);
+            
+
+        Console.ReadKey();
         }
     }
 }
